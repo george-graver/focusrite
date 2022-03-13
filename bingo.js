1dfb78e50fccd7093e6ba5b81fe2d641ba2f9231
George <v4ijdi04ju7l@blurmail.net>
2022-03-13 20:21:53 +0000

    Add method comments

--- bingo.js
+++ bingo.js
@@ -1,3 +1,8 @@
+/**
+ * Parses a raw input string and returns an object with bingo numbers/boards.
+ * @param {*} input 
+ * @returns 
+ */
 function parseInput(input) {
     const inputSections = input.split('\r\n\r\n');
     const numbers = parseNumbers(inputSections[0], ',');
@@ -5,6 +10,11 @@ function parseInput(input) {
     return { numbers, boards };
 }
 
+/**
+ * Parses a board string and returns an object with row/column arrays.
+ * @param {*} boardStr 
+ * @returns 
+ */
 function parseBoard(boardStr) {
     const boardLines = boardStr.split('\r\n');
     const rows = boardLines.map(line => parseNumbers(line, ' '));
@@ -12,6 +22,12 @@ function parseBoard(boardStr) {
     return { rows, columns };
 }
 
+/**
+ * Parses a string representing a delimited sequence of numbers and returns a number array.
+ * @param {*} numbersStr 
+ * @param {*} delimiter 
+ * @returns 
+ */
 function parseNumbers(numbersStr, delimiter) {
     return numbersStr
         .split(delimiter)
@@ -19,15 +35,33 @@ function parseNumbers(numbersStr, delimiter) {
         .filter(n => !isNaN(n));
 }
 
+/**
+ * Inverts a 2d array (i.e. returns each column as a row and vice versa).
+ * @param {*} arr 
+ * @returns 
+ */
 function invert2dArray(arr) {
     return arr.map((row, rowIndex) => row.map((_, colIndex) => arr[colIndex][rowIndex]));
 }
 
+/**
+ * Determines if the given board contains a row or column
+ * where every element appears in the given number array. 
+ * @param {*} numbers 
+ * @param {*} board 
+ * @returns 
+ */
 function isWinningBoard(numbers, board) {
     const combinations = [...board.rows, ...board.columns];
     return combinations.some(c => c.every(n => numbers.includes(n)));
 }
 
+/**
+ * Iterates the given number array and returns the first board which contains a winning row or column.
+ * @param {*} numbers 
+ * @param {*} boards 
+ * @returns 
+ */
 function findFirstWinner(numbers, boards) {
     const calledNumbers = [];
     

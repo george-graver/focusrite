c122ef6f0c8efdd55cb66fe8994cdb4d56d8a3a2
George <v4ijdi04ju7l@blurmail.net>
2022-03-13 18:18:48 +0000

    Check if board is winner

--- main.js
+++ main.js
@@ -17,6 +17,10 @@ function invert2dArray(arr) {
     return arr.map((row, rowIndex) => row.map((_, colIndex) => arr[colIndex][rowIndex]));
 }
 
+function isWinningBoard(numbers, combinations) {
+    return combinations.some(c => c.every(n => numbers.includes(n)));
+}
+
 const lines = parseInput('./input.txt');
 const numbersStr = lines[0];
 const numbers = parseNumbers(numbersStr, ',');
@@ -26,6 +30,7 @@ const boardLines = lines.slice(2);
 const rows = boardLines.map(line => parseNumbers(line, ' '));
 const columns = invert2dArray(rows);
 const possibleCombinations = rows.concat(columns);
-console.log('Possible combinations:', possibleCombinations);
+const isWinner = isWinningBoard(numbers, possibleCombinations);
+console.log("Winner: " + isWinner);
 
 module.exports = { parseNumbers };
